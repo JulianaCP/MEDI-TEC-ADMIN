3d2ed4678737118fc74a7ebb6bf3e99a1f504b5d
Juliana Campos <JCP@hotmail.com>
2017-05-16 21:58:05 -0600

    cambio
    cambio

---
+++ MEDI-TEC-ADMIN/app/src/main/java/com/example/pruebas/diseno/asignar_sintoma_enfermedad.java
@@ -0,0 +1,13 @@
+package com.example.pruebas.diseno;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class asignar_sintoma_enfermedad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asignar_sintoma_enfermedad);
+    }
+}
